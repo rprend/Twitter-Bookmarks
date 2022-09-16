96b0f90ea708de256a361f8e45328051e97ed270
Ryan Prendergast <rprendergast1121@gmail.com>
2022-09-15 23:34:18 -0400

    Connect to OAuth2.0 endpoints and access bookmarks

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,26 @@ import TwitterProvider from "next-auth/providers/twitter"
 export const authOptions = {
   providers: [
   TwitterProvider({
-    clientId: process.env.API_KEY,
-    clientSecret: process.env.API_SECRET
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    version: "2.0",
+    authorization: {
+      url: "https://twitter.com/i/oauth2/authorize",
+      params: {
+        scope: "users.read tweet.read offline.access bookmark.read",
+      },
+    },
+
   })
-  ]
+],
+callbacks: {
+  async jwt({ token, user, account, profile, isNewUser }) {
+    console.log(token)
+    console.log(account)
+    return token
+  }
+
+}
 }
 
 export default NextAuth(authOptions)
