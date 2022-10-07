a3739076388b4646fda23dde6bd059655eec5029
Ryan Prendergast <rprendergast1121@gmail.com>
2022-10-06 22:41:43 -0400

    Email: Switch client from Sendgrid to Postmark for deliverability

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,9 +1,10 @@
-const sgMail = require('@sendgrid/mail');
+const postmark = require("postmark");
 import { getToken } from "next-auth/jwt"
 
 export default async function email(req, res) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
+    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    var client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
+    
     const bookmarks = await search(req, res)
     
     const formatted_bookmarks = bookmarks.data.map((bookmark) => {
@@ -12,21 +13,31 @@ export default async function email(req, res) {
 
     const body_string = formatted_bookmarks.join("\n\n")
 
-    const msg = {
-        to: 'rprendergast1121@gmail.com', 
-        from: 'rprendergast1121@gmail.com',
-        subject: 'Sending with SendGrid is Fun',
-        text: body_string,
-        html: body_string
-    }
-    sgMail
-    .send(msg)
-    .then(() => {
-        res.status(200)
-    })
-    .catch((error) => {
-        res.send(error)
-    })  
+
+    client.sendEmail({
+        "From": "newsletter@tweetnewsletter.xyz",
+        "To": "newsletter@tweetnewsletter.xyz",
+        "Subject": "Test",
+        "TextBody": body_string
+    });
+    
+    console.log("Email sent")
+
+    // const msg = {
+    //     to: 'rprendergast1121@gmail.com', 
+    //     from: 'rprendergast1121@gmail.com',
+    //     subject: 'Sending with SendGrid is Fun',
+    //     text: body_string,
+    //     html: body_string
+    // }
+    // sgMail
+    // .send(msg)
+    // .then(() => {
+    //     res.status(200)
+    // })
+    // .catch((error) => {
+    //     res.send(error)
+    // })  
 
     res.status(200)
   
