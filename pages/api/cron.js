7db4ff11e083d9edd24c7b331c902d30e4896a1e
Ryan Prendergast <rprendergast1121@gmail.com>
2022-10-13 21:04:57 -0400

    Email: Responsive template, twitter embeds, and async behavior

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,11 +1,15 @@
 
 import supabase from '/utils/supabase'
+import mjml2html from 'mjml'
 const postmark = require("postmark");
 
 const TESTING_ENABLED = false
 
 const SENDER_EMAIL = "newsletter@tweetnewsletter.xyz"
+const EMAIL_HEADER = "'s forgotten tweets"
 
+const OEMBED_ENDPOINT = "https://publish.twitter.com/oembed"
+const TWITTER_URL_FORMAT = "https://twitter.com/twitter/status/"
 // Our table has columns:
 // created_at 
 // user 
@@ -31,33 +35,75 @@ async function read_database() {
   return data
 }
 
+// This is a handy little markup language that handles making a responsive email template
+function generate_html_email(tweets) {
+  const htmlOutput = mjml2html(`
+  <mjml>
+    <mj-body>
+      <!-- Intro -->
+      <mj-section backgrou>
+        <mj-column>
+          <mj-text>
+            here's this week's tweets
+          </mj-text>
+        </mj-column>
+      </mj-section>
+
+      <!-- Tweets -->
+      <mj-section>
+      <mj-column>
+      ${
+        tweets.map(tweet => {
+          return `
+          <mj-text>
+            ${tweet}
+          </mj-text>
+          `
+        })
+      }
+      </mj-column>
+      </mj-section>
+    </mj-body>
+  </mjml>
+`)
+
+return htmlOutput.html
+}
+
+// Rewritten to return a promise so we can wait for the oembed call to finish
 function generate_email(row) {
   const tweets = row.tweet_list
 
-  // Format the tweets into a string
-  var tweet_string = ""
-  for (var tweet of tweets) {
-    tweet_string += "Tweet ID: " + tweet.id + "\n" + tweet.text + "\n\n"
-  }
-  return tweet_string
+  var tweets_html_promises = tweets.map(tweet => {
+    // Twitter has a nice Embed endpoint to get the HTML for a tweet.
+    return fetch(OEMBED_ENDPOINT + "?url=" + TWITTER_URL_FORMAT + tweet.id)
+      .then(response => response.json())
+  })
+
+  return Promise.all(tweets_html_promises).then((values) => {
+    const html_tweets = values.map(tweet => tweet.html)
+    return generate_html_email(html_tweets)
+  })
 }
 
-function send_emails(data) {
-  var email_list = []
-  for (const row of data) {
-      const email_body = generate_email(row)
-      email_list.push({
+async function send_emails(data) {
+  var email_promises = data.map(row => {
+    return generate_email(row).then(email_body => {
+      return {
         "From": SENDER_EMAIL,
         "To": row.email,
-        "Subject": row.user + "'s Tweets",
-        "TextBody": email_body
-      })
-  }
+        "Subject": row.user + EMAIL_HEADER,
+        "HtmlBody": email_body
+      }
+    })
+  })
 
-  const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
-  for (var email of email_list) {
-    client.sendEmail(email);
-  }
+  return Promise.all(email_promises).then((email_list) => {  
+    const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
+    client.sendEmailBatch(email_list).then(function (response) {
+      console.log(response);
+    })
+  })
 
 } 
 
@@ -73,16 +119,17 @@ export default async function handler (req, res) {
     const data = await read_database()
     send_emails(data)
 
-    res.status(200).json({ data: 'Emails sent' })
+    res.status(200).json({ data: 'Emails queued, check logs for any errors' })
 
 
   } else {
 
     // FOR TESTING ONLY, DO NOT USE IN PRODUCTION
-    // This is a hacky way to test out the cron job. Do not use 
+    // This is a hacky way to test out the cron job. Do not use. i'm watching u 
     if (TESTING_ENABLED) {
       const a = await read_database()
       send_emails(a)
+
       res.json(a)
       return
     }
